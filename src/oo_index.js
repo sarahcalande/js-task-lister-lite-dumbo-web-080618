2e7791f6bef130a094810c62090463f85913302e
sarahcalande <scalande@yahoo.com>
2018-10-10 14:16:33 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,18 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
+
+
+//find task creator form
+  let taskcreator = document.querySelector("#create-task-form")
+
+//add event listener so that input appears where its supposed to on submit
+  taskcreator.addEventListener("submit", function(a){
+      a.preventDefault()
+      let list = document.createElement("li");
+      let tasks = document.querySelector("#tasks");
+      let userinput = document.querySelector("#new-task-description").value;
+      list.innerText = userinput;
+      tasks.appendChild(list)
+  })
 });
